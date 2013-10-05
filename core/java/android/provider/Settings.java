187db848243058a2a4f999d2f43d029ea9495075
maxwen <max.weninger@gmail.com>
2013-10-05 13:15:36 +0200

    [1/3] base: add support for adb over network
    Change-Id: If8354c92a03da501c683638cc69097d287d74816
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15568,6 +15568,12 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
 
+         /**
+         * The TCP/IP port to run ADB on, or 0 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
