54eeffdb74d536e101b4116b99138b4cacf7f466
thecrazyskull <anaskarbila@aospa.co>
2016-07-10 06:32:58 -0400

    Add DASH support.
    ** EDITS **
    Dirty Unicorns: Move boolean method isDashCharger
                    in our Utils class.
    
    Change-Id: I337feabf26ebdca100b651dcf492cc78b4fede74

--- core/java/com/android/internal/util/cherish/CherishUtils.java
+++ core/java/com/android/internal/util/cherish/CherishUtils.java
@@ -49,6 +49,11 @@ import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
 import android.util.DisplayMetrics;
 import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Locale;
 public class CherishUtils {
 	
@@ -173,6 +178,21 @@ public class CherishUtils {
                 }
             }
         }
+    }
+	
+	// Method to detect if device has DASH support.
+    public static boolean isDashCharger() {
+        try {
+            FileReader file = new FileReader("/sys/class/power_supply/battery/fastchg_status");
+            BufferedReader br = new BufferedReader(file);
+            String state = br.readLine();
+            br.close();
+            file.close();
+            return "1".equals(state);
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+        return false;
     }
 
     public static void sendKeycode(int keycode) {
