429c6004c54de2f5601e4113bd6ba6651f5952cf
Christian Oder <myself5@carbonrom.org>
2020-01-07 20:06:13 +0100

    Introduce bold digital and Samsung like lockscreen clocks
    * Idea and clock formatting from Alex <du.alexcruz@gmail.com>
    * Credits to shagbag913 <sh4gbag913@gmail.com> for the thumbnails
    
    Change-Id: I00a6acc1137df76072bb1ab83abd81cb1d8b8228
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -148,6 +148,8 @@ public final class ClockManager {
         LayoutInflater layoutInflater = injectionInflater.injectable(LayoutInflater.from(context));
 
         addBuiltinClock(() -> new SamsungHighlightClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new SamsungClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new SamsungBoldClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new BinaryClockController(res, layoutInflater, colorExtractor));
         //addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
