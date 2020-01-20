7ffff7524efd1d699f72184a20f3dc7958c8fccb
Mazda-- <alex@dirtyunicorns.com>
2020-01-20 21:24:30 +0530

    Themes: Add DU themes [1/3]
    - This adds 'solarized dark' theme
    
    Thanks to bigrushdog <randall.rushing@gmail.com> for helping me figure out
    how to apply different actions based on the item selected, really HUGE help!

--- core/java/com/android/internal/util/cherish/ThemesUtils.java
+++ core/java/com/android/internal/util/cherish/ThemesUtils.java
@@ -1,11 +1,11 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
-/**
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,9 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
--->
-<resources>
-<!-- Themes -->
-    <color name="theme_generic_semi_transparent_color">#EAFFFFFF</color>
-</resources>
+
+package com.android.internal.util.cherish;
+
+public class ThemesUtils {
+
+    public static final String[] SOLARIZED_DARK = {
+            "com.android.theme.solarizeddark.system",
+            "com.android.theme.solarizeddark.systemui",
+    };
+}
 
