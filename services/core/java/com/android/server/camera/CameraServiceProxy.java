PixelExperience/frameworks_base

fwb: Start device specific popup camera service conditionally
Change-Id: I07f97fe58495ef666fa317d14fa1334389015e11

services/core/java/com/android/server/camera/CameraServiceProxy.java
@@ -17,6 +17,7 @@ package com.android.server.camera;
 
 import android.annotation.IntDef;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -53,6 +54,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+
+import com.android.internal.util.custom.popupcamera.PopUpCameraUtils;
 
 /**
  * CameraServiceProxy is the system_server analog to the camera service running in cameraserver.
@@ -128,7 +131,7 @@ public class CameraServiceProxy extends SystemService
     private final @NfcNotifyState int mNotifyNfc;
     private boolean mLastNfcPollState = true;
     private final boolean mAllowMediaUid;
-    private final boolean mSendCameraStatusIntent;
+    private final String mPopUpCameraServiceComponentName;
 
     private long mClosedEvent;
     private long mOpenEvent;
@@ -226,7 +229,7 @@ public class CameraServiceProxy extends SystemService
 
             updateActivityCount(cameraId, newCameraState, facing, clientName, apiLevel);
 
-            if (mSendCameraStatusIntent && facing == ICameraServiceProxy.CAMERA_FACING_FRONT) {
+            if (!mPopUpCameraServiceComponentName.equals("") && facing == ICameraServiceProxy.CAMERA_FACING_FRONT) {
                 switch (newCameraState) {
                    case ICameraServiceProxy.CAMERA_STATE_OPEN : {
                        mOpenEvent = SystemClock.elapsedRealtime();
@@ -262,8 +265,8 @@ public class CameraServiceProxy extends SystemService
         if (DEBUG) Slog.v(TAG, "Notify NFC state is " + nfcNotifyToString(mNotifyNfc));
         mAllowMediaUid = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_allowMediaUidForCameraServiceProxy);
-        mSendCameraStatusIntent = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_sendCameraStatusIntent);
+        mPopUpCameraServiceComponentName = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_popUpCameraServiceComponentName);
     }
 
     @Override
@@ -340,6 +343,18 @@ public class CameraServiceProxy extends SystemService
 
             // Ensure NFC is back on
             notifyNfcService(/*enablePolling*/ true);
+        }
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED &&
+                !mPopUpCameraServiceComponentName.equals("")) {
+            String perm = PopUpCameraUtils.MANAGE_POPUP_CAMERA_SERVICE_PERMISSION;
+            mContext.enforceCallingOrSelfPermission(perm, "Missing or invalid popup camera service permission: " + perm);
+            Intent i = new Intent();
+            i.setComponent(ComponentName.unflattenFromString(mPopUpCameraServiceComponentName));
+            mContext.startServiceAsUser(i, UserHandle.SYSTEM);
         }
     }
 

zahidmgsk/android_frameworks_base-1

services/core/java/com/android/server/camera/CameraServiceProxy.java
@@ -17,6 +17,7 @@ package com.android.server.camera;
 
 import android.annotation.IntDef;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -53,6 +54,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+
+import com.android.internal.util.custom.popupcamera.PopUpCameraUtils;
 
 /**
  * CameraServiceProxy is the system_server analog to the camera service running in cameraserver.
@@ -128,7 +131,7 @@ public class CameraServiceProxy extends SystemService
     private final @NfcNotifyState int mNotifyNfc;
     private boolean mLastNfcPollState = true;
     private final boolean mAllowMediaUid;
-    private final boolean mSendCameraStatusIntent;
+    private final String mPopUpCameraServiceComponentName;
 
     private long mClosedEvent;
     private long mOpenEvent;
@@ -226,7 +229,7 @@ public class CameraServiceProxy extends SystemService
 
             updateActivityCount(cameraId, newCameraState, facing, clientName, apiLevel);
 
-            if (mSendCameraStatusIntent && facing == ICameraServiceProxy.CAMERA_FACING_FRONT) {
+            if (!mPopUpCameraServiceComponentName.equals("") && facing == ICameraServiceProxy.CAMERA_FACING_FRONT) {
                 switch (newCameraState) {
                    case ICameraServiceProxy.CAMERA_STATE_OPEN : {
                        mOpenEvent = SystemClock.elapsedRealtime();
@@ -262,8 +265,8 @@ public class CameraServiceProxy extends SystemService
         if (DEBUG) Slog.v(TAG, "Notify NFC state is " + nfcNotifyToString(mNotifyNfc));
         mAllowMediaUid = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_allowMediaUidForCameraServiceProxy);
-        mSendCameraStatusIntent = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_sendCameraStatusIntent);
+        mPopUpCameraServiceComponentName = mContext.getResources().getString(
+                com.android.internal.R.string.config_popUpCameraServiceComponentName);
     }
 
     @Override
@@ -340,6 +343,18 @@ public class CameraServiceProxy extends SystemService
 
             // Ensure NFC is back on
             notifyNfcService(/*enablePolling*/ true);
+        }
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED &&
+                !mPopUpCameraServiceComponentName.equals("")) {
+            String perm = PopUpCameraUtils.MANAGE_POPUP_CAMERA_SERVICE_PERMISSION;
+            mContext.enforceCallingOrSelfPermission(perm, "Missing or invalid popup camera service permission: " + perm);
+            Intent i = new Intent();
+            i.setComponent(ComponentName.unflattenFromString(mPopUpCameraServiceComponentName));
+            mContext.startServiceAsUser(i, UserHandle.SYSTEM);
         }
     }
 