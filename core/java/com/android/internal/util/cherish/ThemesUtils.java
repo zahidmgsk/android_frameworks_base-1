204f490acea9fefa809c38627300de21f4eecf97
mahmoudk1000 <mahmoudk1000@gmail.com>
2020-04-08 00:45:35 +0000

    Add MaterialOcean System Theme [2/3]

--- core/java/com/android/internal/util/cherish/ThemesUtils.java
+++ core/java/com/android/internal/util/cherish/ThemesUtils.java
@@ -42,5 +42,10 @@ public class ThemesUtils {
             "com.android.theme.darkgrey.system",
             "com.android.theme.darkgrey.systemui",
     };
+	
+	public static final String[] MATERIAL_OCEAN = {
+            "com.android.theme.materialocean.system",
+            "com.android.theme.materialocean.systemui",
+    };
 }
 
