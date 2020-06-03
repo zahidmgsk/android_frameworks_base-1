a58a181863f979d534d8cbe22e58142c243a1c5b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-03 10:22:30 -0300

    Revert "SystemUI: Conditionally hide extra biometric help on keyguard"
    This reverts commit 9884375bdd5bf2a73f318eedc62c0b7bda7d3931.
    
    Change-Id: I6d57bf3b7ec269464d5da35e1edfd3a50a43b17c

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -132,8 +132,6 @@ public class KeyguardIndicationController implements StateListener,
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
-    private boolean mBiometricHelpShowOnlyWhenFailed = false;
-
     private final DevicePolicyManager mDevicePolicyManager;
     private boolean mDozing;
     private final ViewClippingUtil.ClippingParameters mClippingParams =
@@ -204,8 +202,6 @@ public class KeyguardIndicationController implements StateListener,
         mKeyguardUpdateMonitor.registerCallback(mTickReceiver);
         mStatusBarStateController.addCallback(this);
         mUnlockMethodCache.addListener(this);
-
-        mBiometricHelpShowOnlyWhenFailed = res.getBoolean(R.bool.config_biometricHelpShowOnlyWhenFailed);
     }
 
     public void setIndicationArea(ViewGroup indicationArea) {
@@ -783,9 +779,6 @@ public class KeyguardIndicationController implements StateListener,
         @Override
         public void onBiometricHelp(int msgId, String helpString,
                 BiometricSourceType biometricSourceType) {
-            if (mBiometricHelpShowOnlyWhenFailed && msgId != -1){
-                return;
-            }
             if (!mKeyguardUpdateMonitor.isUnlockingWithBiometricAllowed()) {
                 return;
             }
