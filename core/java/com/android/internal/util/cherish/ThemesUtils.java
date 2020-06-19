bbab1163a2217afd444a05cda8968ae392e0cade
Hưng Phan <phandinhhungvp2001@gmail.com>
2020-06-19 08:44:01 +0700

    base:fix defaut switch styles

--- core/java/com/android/internal/util/cherish/ThemesUtils.java
+++ core/java/com/android/internal/util/cherish/ThemesUtils.java
@@ -1,18 +1,7 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/* * Copyright (C) 2014 The Android Open Source Project * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the 
+ License. * You may obtain a copy of the License at * * http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed 
+ under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing 
+ permissions and * limitations under the License. */
 
 package com.android.internal.util.cherish;
 
@@ -67,7 +56,7 @@ public class ThemesUtils {
     };
 
     public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {
-        if (switchStyle == 1) {
+        if (switchStyle == 2) {
             stockSwitchStyle(om, userId);
         } else {
             try {
@@ -91,4 +80,3 @@ public class ThemesUtils {
         }
     }
 }
-
