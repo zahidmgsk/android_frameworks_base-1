e79a594859183f49415116b1190a6e33922366db
Hưng Phan <phandinhhungvp2001@gmail.com>
2020-06-26 22:06:03 +0700

    Hide Things below VolumePanel [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6252,6 +6252,12 @@ public final class Settings {
          */
         public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
 
+          /**
+           * Whether show/hide things in volume panel
+           * @hide
+           */
+          public static final String HIDE_THINGS_VOLUMEPANEL = "hide_things_volumepanel";
+
         /**
          * Settings dashboard icon style
          * @hide
