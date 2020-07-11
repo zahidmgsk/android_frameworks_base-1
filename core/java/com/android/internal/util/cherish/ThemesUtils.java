34d72d118045e27c6e6556fa9a8a5f707560b3ee
Hưng Phan <phandinhhungvp2001@gmail.com>
2020-07-11 22:29:16 +0700

    themes:Add Clear Spring Theme [1/3]

--- core/java/com/android/internal/util/cherish/ThemesUtils.java
+++ core/java/com/android/internal/util/cherish/ThemesUtils.java
@@ -1,6 +1,11 @@
-/* * Copyright (C) 2014 The Android Open Source Project * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the 
- License. * You may obtain a copy of the License at * * http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed 
- under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing 
+/* * Copyright (C) 2014 The Android Open Source Project * * Licensed 
+ under the Apache License, Version 2.0 (the "License"); * you may not 
+ use this file except in compliance with the License. * You may obtain a 
+ copy of the License at * * http://www.apache.org/licenses/LICENSE-2.0 * 
+ * Unless required by applicable law or agreed to in writing, software * 
+ distributed under the License is distributed on an "AS IS" BASIS, * 
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ implied. * See the License for the specific language governing 
  permissions and * limitations under the License. */
 
 package com.android.internal.util.cherish;
@@ -42,6 +47,11 @@ public class ThemesUtils {
 	public static final String[] MATERIAL_OCEAN = {
             "com.android.theme.materialocean.system",
             "com.android.theme.materialocean.systemui",
+    };
+	
+	public static final String[] CLEAR_SPRING = {
+            "com.android.theme.clearspring.system",
+            "com.android.theme.clearspring.systemui",
     };
 	
 	private static final String[] QS_TILE_THEMES = {
