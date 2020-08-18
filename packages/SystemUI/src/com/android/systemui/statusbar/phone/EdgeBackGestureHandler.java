ezio84/abc-n_frameworks_base

Gesture nav toggle: show IME when needed and improve code
keeping the frame height to stock value, and setting to 0 the standard
height only, let apps to still set proper insets like there's no navbar,
but allows the navbar to expand itself when IME button shows up.

Also, in EdgeBackGestureHandler, we keep navbar height value to stock,
that's used to check if the back swipe touch event is within touch
region.
This avoid triggering the back gesture when instead the user wants
to swipe on the (hidden) navbar to scroll recents

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -210,8 +210,11 @@ public class EdgeBackGestureHandler implements DisplayListener {
         mLongPressTimeout = Math.min(MAX_LONG_PRESS_TIMEOUT,
                 ViewConfiguration.getLongPressTimeout());
 
-        mNavBarHeight = Utils.shouldShowGestureNav(context) ?
-                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height) : 0;
+        // always consider the normal navbar height even if nav is hidden,
+        // so when swiping at the very bottom of the screen to scroll recents apps
+        // the back swipe action is not triggered too at the same time (see isWithinTouchRegion).
+        mNavBarHeight = /*Utils.shouldShowGestureNav(context) ?
+                */res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height)/* : 0*/;
         mMinArrowPosition = res.getDimensionPixelSize(R.dimen.navigation_edge_arrow_min_y);
         mFingerOffset = res.getDimensionPixelSize(R.dimen.navigation_edge_finger_offset);
         updateCurrentUserResources(res);

zahidmgsk/android_frameworks_base-1

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -206,8 +206,11 @@ public class EdgeBackGestureHandler implements DisplayListener, TunerService.Tun
         mLongPressTimeout = Math.min(MAX_LONG_PRESS_TIMEOUT,
                 ViewConfiguration.getLongPressTimeout());
 
-        mNavBarHeight = IonUtils.shouldShowGestureNav(context) ?
-                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height) : 0;
+        // always consider the normal navbar height even if nav is hidden,
+        // so when swiping at the very bottom of the screen to scroll recents apps
+        // the back swipe action is not triggered too at the same time (see isWithinTouchRegion).
+        mNavBarHeight = /*IonUtils.shouldShowGestureNav(context) ?
+                */res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height)/* : 0*/;
         mMinArrowPosition = res.getDimensionPixelSize(R.dimen.navigation_edge_arrow_min_y);
         mFingerOffset = res.getDimensionPixelSize(R.dimen.navigation_edge_finger_offset);
         updateCurrentUserResources(res);